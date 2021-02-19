b97e1adb141d7c4089756a346fb19f7b83f3b9d5
ISMAKOVA <48486359+ismakova@users.noreply.github.com>
2021-02-19 22:03:01 +0500

    добавлена функция, меняющая конечные знаки препинания на теги

--- preprocessing.py
+++ preprocessing.py
@@ -10,7 +10,7 @@ noticed that they were filled with cupboards and book-shelves; here and there sh
 saw maps and pictures hung upon pegs. She took down a jar from one of the shelves
 as she passed; it was labelled 'ORANGE MARMALADE', but to her great disappointment it
 was empty: she did not like to drop the jar for fear of killing somebody, so managed
-to put it into one of the cupboards as she fell past it. Hello my friend, it is me, the man from France.'''
+to put it into one of the cupboards as she fell past it. Hello my friend, it is me, the man from France!'''
 
 
 def prep(text):
@@ -22,7 +22,17 @@ def prep(text):
         lemm_token_text.append(lemm_sent)
     return lemm_token_text
 
-# def adding_tags(sentences_arr):
-#     tags = {"ы": "<S>", ".": "</S>", "!": "<!/S>", "?": "<?/S>", "!?": "<?!/S>","z":"<Z>", "q": "<Q>", "and or": "&" }
 
-print(prep(para))
+def adding_start_end_tags(sentences_arr):
+    sent_with_tags = [' '.join(['<S>', sent.replace('.', '</S>')
+                            .replace('!', '<!/S>').replace('?', '<?/S>')
+                            .replace('!?', '<?!/S>')])
+                      for sent in sentences_arr]
+
+    return sent_with_tags
+
+
+sentences_arr = prep(para)
+print(sentences_arr)
+sent_with_tags = adding_start_end_tags(sentences_arr)
+print(sent_with_tags)
