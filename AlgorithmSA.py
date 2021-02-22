9104b8e98f1ab81f5489c3c73d7693d75b57809a
ISMAKOVA <48486359+ismakova@users.noreply.github.com>
2021-02-22 20:29:50 +0500

    Алгоритм работает только без деления на клауземы

--- AlgorithmSA.py
+++ AlgorithmSA.py
@@ -2,6 +2,8 @@ import nltk
 import spacy
 import deplacy
 import re
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 
 nlp = spacy.load('en_core_web_sm')
 
@@ -13,26 +15,34 @@ def replace_end_symbols(text):
 
 # replacing symbols in inner parts
 def replace_inner_symbols(text):
-    text_replaced = text.replace('"', '<Q>').replace(',', '<Z>').replace(':', '<Z>').replace(';', '<Z>').replace('-', '<Z>')
-    return re.sub(r'[\ ]or|[\ ]and', ' CJ', text_replaced)
+    return text.replace('"', '<Q>').replace(',', '<Z>').replace(':', '<Z>').replace(';', '<Z>').replace('-', '<Z>')
 
 
-def mark_caps(splitted_sentence):
+# mark words written in caps
+def mark_caps_CJ(sentence):
     marked_text = []
-    for word in splitted_sentence:
+    for word in sentence.split():
         if word.isupper() and word != 'CJ' and word != 'I':
             word = ''.join([word, '<CAP>'])
             marked_text.append(word)
         else:
             marked_text.append(word)
-    return marked_text
+    return re.sub(r'[\ ]or|[\ ]and', ' CJ', ' '.join(marked_text))
 
 
+def lemmatization(sentence):
+    lemm_sent = []
+    for word in nlp(sentence):
+        if word.text.isupper():
+            lemm_sent.append(word.lemma_.upper())
+        else:
+            lemm_sent.append(word.lemma_)
+    return ' '.join(lemm_sent)
 
-#dividing sentence into clause, but only for Compound Sentences
+# dividing sentence into clause, but only for Compound Sentences
 def divide_into_clauses(sentence):
     doc = nlp(sentence)
-    #deplacy.render(doc)
+    # deplacy.render(doc)
     seen = set()
     clauses = []
 
@@ -61,21 +71,36 @@ def divide_into_clauses(sentence):
     return [cl for i, cl in clauses]
 
 
-exampleq = 'The CIA has been incompetent from its inception. ' \
+def algorithm_sa_without_clauses(text):
+    sentences = nltk.sent_tokenize(text)
+    lemm_text = [lemmatization(sentence) for sentence in sentences]
+    # print(lemm_text)
+    marked_text = [mark_caps_CJ(sentence) for sentence in lemm_text]
+    # print(marked_text)
+    tagged_end_text = [replace_end_symbols(sentence) for sentence in marked_text]
+    # print(tagged_end_text)
+    tagged_inner_text = [replace_inner_symbols(sentence) for sentence in tagged_end_text]
+    count_words = sum([len(sentence.split()) for sentence in marked_text])
+    print(count_words)
+    return tagged_inner_text
+
+
+example = 'The CIA has been incompetent from its inception. ' \
           'The roster of incompetence includes subversion operations that cost the lives of hundreds of agents and accomplished nothing; ' \
           'CIA-managed coups that backfired, the Bay of Pigs and many others. ' \
           'Even operations that "succeeded" were pyrrhicThe CIA has been incompetent from its inception. ' \
           'I stopped believing in Santa Claus when my mother took me to see him in a department store, and he asked for my autograph. ' \
           'When you write a comic strip, the person on the left always speaks first. ' \
           'I used to jog, but the ice cubes kept falling out of my glass. '
-example ='The CIA has been incompetent from its inception. The roster of incompetence includes subversion operations that cost the lives of hundreds of agents and accomplished nothing; CIA-managed coups that backfired, the Bay of Pigs and many others. Even operations that "succeeded" were pyrrhic'
-sentences = nltk.sent_tokenize(example)
-print('-----------tagged_end_text------------')
-tagged_end_text = [replace_end_symbols(sentence) for sentence in sentences]
-print(tagged_end_text)
-print('-----------tagged_inner_text------------')
-tagged_inner_text = [replace_inner_symbols(sentence) for sentence in tagged_end_text]
-print(tagged_inner_text)
+example2 = 'The CIA has been incompetent from its inception. The roster of incompetence includes subversion operations that cost the lives of hundreds of agents and accomplished nothing; CIA-managed coups that backfired, the Bay of Pigs and many others. Even operations that "succeeded" were pyrrhic'
+result = algorithm_sa_without_clauses(example)
+print(result)
+# print('-----------tagged_end_text------------')
+# tagged_end_text = [replace_end_symbols(sentence) for sentence in sentences]
+# print(tagged_end_text)
+# print('-----------tagged_inner_text------------')
+# tagged_inner_text = [replace_inner_symbols(sentence) for sentence in tagged_end_text]
+# print(tagged_inner_text)
 
 ################ for clauses #########################
 # print('-----------divide_into_clauses------------')
@@ -88,11 +113,11 @@ print(tagged_inner_text)
 #####################################################
 
 
-print('-----------marked_CAPS_text ans splitted ------------')
-marked_text = [mark_caps(sentence.split()) for sentence in tagged_inner_text]
-print(marked_text)
-
-
-print('-----------count_words------------')
-count_words = sum([len(sentence) for sentence in marked_text])
-print(count_words)
+# print('-----------marked_CAPS_text ans splitted ------------')
+# marked_text = [mark_caps_CJ(sentence.split()) for sentence in tagged_inner_text]
+# print(marked_text)
+#
+#
+# print('-----------count_words------------')
+# count_words = sum([len(sentence) for sentence in marked_text])
+# print(count_words)
